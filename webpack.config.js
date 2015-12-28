bdf69ff7d34353168963758f845b886d490ff7a1
Felipe Apostol <flipjs.io@gmail.com>
2015-12-28 15:29:16 +0000

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  entry: './main.js',
+  output: {
+    path: './',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    inline: true,
+    port: 3000
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+        query: {
+          presets: ['react', 'es2015', 'stage-0', 'stage-2']
+        }
+      },
+      {
+        test: /\.css$/,
+        loader: 'style!css'
+      }
+    ]
+  }
+}
